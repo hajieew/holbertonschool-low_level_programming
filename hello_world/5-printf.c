2d718f8f6a192435ac67ad6ad1b5f7a6e2328d57
hajieew <hajieew@gmail.com>
2025-11-28 10:44:34 +0000

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+
+/**
+ * main - Prints the phrase:
+ * "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
