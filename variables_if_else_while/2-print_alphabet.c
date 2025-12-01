653d2caa4f537603627cab4fc99c0ed41835b6c7
hajieew <hajieew@gmail.com>
2025-12-01 14:16:12 +0000

    task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char s  = 'a';
 
-	while(s < 'z')
+	while(s <= 'z')
 	{
 		putchar(s);
 		s++;
