9402464314d354e9772fd1db7886958202b1615a
hajieew <hajieew@gmail.com>
2025-12-01 12:59:04 +0000

    task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("is %d and is greater than 5\n", n);
 	else if (n % 10 == 0)
 		printf("is %d and is 0\n", n);
-	else if (n % 10 < n) && (n % 10 != 0)
+	else if (n % 10 < 6) && (n % 10 != 0)
 		printf("is %d and is less than 6 and not 0\n", n);
 
 	return (0);
