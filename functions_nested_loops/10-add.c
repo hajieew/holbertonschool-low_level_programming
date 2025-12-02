eaab9d2e3866a7ad7eccda5d6c5449840daeddf8
hajieew <hajieew@gmail.com>
2025-12-02 14:31:29 +0000

    python

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.c"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
