31f7287500608515d6d4bd9906f6231b71101bcf
hajieew <hajieew@gmail.com>
2025-12-02 13:57:26 +0000

    python

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer to compute
+ *
+ * Return: absolute value of n
+ */
+
 int _abs(int n)
 {
 	if (n>0)
