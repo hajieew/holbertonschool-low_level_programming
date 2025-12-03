b21468f5b77854fc7d06713e782bdd2ae6cd7b8f
hajieew <hajieew@example.com>
2025-12-03 08:36:19 +0000

    python

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,12 @@
 /**
  * _isdgit - checks if input is number or not
  * @c: the character to check
+ *
  * Return: 1 if input is numbers, otherwise 0
  */
 int _isdigit(int c)
 {
-	if ((c >= 0) && (c <= 9))
+	if ((c >= '0') && (c <= '9'))
 		return (1);
 
 	return (0);
