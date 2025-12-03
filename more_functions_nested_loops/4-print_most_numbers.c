640ab7d681088d7ea31998a010f3c175de9cbbb3
hajieew <hajieew@example.com>
2025-12-03 09:12:08 +0000

    python

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9 except 2 and 4
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
  */
 void print_most_numbers(void)
 {
 	int a = 0;
 
-	if ((a >= 0) && (a <= 9) && (a != 2) && (a != 4))
+	while ((a >= 0) && (a <= 9))
 	{
-		_putchar(a + '0');
-		a++;
+		if ((a != 2) && (a != 4))
+		{
+			_putchar(a + '0');
+			a++;
+
+		}
 	}
 	_putchar('\n');
 }
