fc68554c1823fce47aa34dad2340774b071dee94
hajieew <hajieew@example.com>
2025-12-03 09:29:40 +0000

    python

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ */
+void more_numbers(void)
+{
+	int a;
+	int b = 0;
+	for (a = 0; a <= 9; a++)
+	{
+		while (b <= 14)
+		{
+		_putchar(b + '0');
+			b++;
+		}
+	}
+}
