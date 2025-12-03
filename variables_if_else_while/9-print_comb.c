11911fa5e16234214a0606741ad59cd27e4bd4b6
hajieew <hajieew@example.com>
2025-12-03 09:51:32 +0000

    python

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 	int i;
 	for (i = 0; i <= 9; i++)
 	{
-		putchar(i + '0')
+		putchar(i + '0');
 		if (i != 9)
 			putchar(", ")
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
