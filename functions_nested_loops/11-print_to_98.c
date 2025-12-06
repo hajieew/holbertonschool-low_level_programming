d3ed79e59cacfef8af4618326cf0d6b439ed5b87
hajieew <hajieew@gmail.com>
2025-12-06 17:07:38 +0000

    python

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ *
+ * Description: prints numbers separated by comma and space,
+ *              ending with 98 followed by a new line.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+
+	printf("98\n");
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -11,4 +11,5 @@ int _abs(int n);
 int print_last_digit(int n);
 int add(int, int);
 void times_table(void);
+void print_to_98(int n);
 #endif
