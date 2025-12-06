cdf342a6075ba470d627491a47cb34487883ed23
hajieew <hajieew@gmail.com>
2025-12-06 16:43:46 +0000

    python

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,14 +5,14 @@
  */
 void time_table(void)
 {
-	int a,b
+	int a,b;
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 			printf("%d", a * b);
 
-		if (b < 9)
-			printf(", ");
+			if (b < 9)
+				printf(", ");
 	}
 	printf("\n");
 }
