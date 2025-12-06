f92d61d7a1d77b3e181ca34f8419256263b8c2fa
hajieew <hajieew@gmail.com>
2025-12-06 17:18:41 +0000

    python

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		while (size !=  0)
+		{
+			_putchar('#');
+			size--;
+		}
+	}
+	else
+		_putchar('\n')
+
+	_putchar('\n')
+}
