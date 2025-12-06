0699bc8acba27506adaa8ff4aaa9b3a6c48f350b
hajieew <hajieew@gmail.com>
2025-12-06 16:51:22 +0000

    python

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,22 +1,22 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  *
  */
 void times_table(void)
 {
-	int a,b;
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			printf("%d", a * b);
+    int a, b;
 
-			if (b < 9)
-				printf(", ");
-		}
-	}
-	printf("\n");
+    for (a = 0; a <= 9; a++)
+    {
+        for (b = 0; b <= 9; b++)
+        {
+            print_number(a * b);
+
+            if (b < 9)
+                write(1, ", ", 2);
+        }
+        write(1, "\n", 1);
+    }
 }
-
