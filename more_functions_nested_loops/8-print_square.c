0a7b127138d29dc6e7aa88503dfd7695b967eef9
hajieew <hajieew@gmail.com>
2025-12-06 17:20:31 +0000

    python

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
 /**
+ * print_square - prints a square, followed by a new line
+ * @size: size of the square
  *
+ * Description: Prints a square of '#' characters with dimensions size x size.
+ * If size is 0 or less, prints only a new line.
  */
-
 void print_square(int size)
 {
+	int a, b;
+
 	if (size > 0)
 	{
-		while (size !=  0)
+		for (a = 0; a < size; a++)
 		{
-			_putchar('#');
-			size--;
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 	}
 	else
-		_putchar('\n')
-
-	_putchar('\n')
+		_putchar('\n');
 }
