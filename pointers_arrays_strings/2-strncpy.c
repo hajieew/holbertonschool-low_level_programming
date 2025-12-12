742851aa1af0fe5441f33d4bdc89a4ddf922b0a4
hajieew <hajieew@gmail.com>
2025-12-12 12:31:19 +0000

    python

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination buffer
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
