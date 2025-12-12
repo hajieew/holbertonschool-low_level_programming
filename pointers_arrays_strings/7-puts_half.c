feb2398293bcfe62db2234a333463080b02c5cd3
hajieew <hajieew@gmail.com>
2025-12-12 12:09:17 +0000

    python

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to the string
+ */
+void puts_half(char *str)
+{
+	int a = 0;
+	int i;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+
+	i = (a + 1) / 2;
+
+	while (i < a)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n')
