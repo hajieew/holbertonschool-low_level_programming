d83e2e82984ca518138b0673edfa2731e970a031
hajieew <hajieew@gmail.com>
2025-12-12 13:54:13 +0000

    python

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: string containing bytes to match
+ *
+ * Return: pointer to first occurrence in s of any byte from accept, or NULL if none found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+				return (&s[a]);
+		}
+	}
+
+	return (NULL);
+}
