80907d36fbb367537caeb645e84eef0c81ebf37f
hajieew <hajieew@gmail.com>
2025-12-16 10:53:17 +0000

    python

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /*
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to the string
  *
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
