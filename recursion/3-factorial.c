f661a0bea8487222ac2b52c4e47142698a91a93d
hajieew <hajieew@gmail.com>
2025-12-16 11:00:25 +0000

    python

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
+ * factorial - returns the factorial of a given number
+ * @n: number to calculate the factorial of
  *
+ * Return: factorial of n, or -1 if n < 0
  */
 int factorial(int n)
 {
@@ -9,5 +12,5 @@ int factorial(int n)
 		return (-1);
 	if (n == 0)
 		return (1);
-	return (n * factorial(n-1));
+	return (n * factorial(n - 1));
 }
