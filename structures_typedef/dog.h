124e5bbb9444cb1409cc11299fca499bab0ed846
hajieew <hajieew@gmail.com>
2026-01-03 16:15:27 +0000

    python

---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/* Typedef for struct dog */
+typedef struct dog dog_t;
+
+/* Function prototypes */
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
