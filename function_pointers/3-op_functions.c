c0f2b6d5519b4e33d3816f1b2971857ebd4df839
hajieew <hajieew@gmail.com>
2026-01-03 16:50:18 +0000

    python

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts b from a
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of a / b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
