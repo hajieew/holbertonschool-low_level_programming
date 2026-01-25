5ace2222b996b65cf29a2e54f6841ae78a41e096
hajieew <hajieew@gmail.com>
2026-01-25 13:58:05 +0000

    python

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		returun (-1);
+		return (-1);
 
 	if (len > 0)
 	{
