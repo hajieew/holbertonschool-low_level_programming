c982cd5c34a27dd2591a9c4670930b8326985b66
hajieew <hajieew@gmail.com>
2026-01-25 13:53:59 +0000

    python

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,59 +1,44 @@
 #include "main.h"
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to POSIX stdout
- * @filename: name of the file
- * @letters: number of characters to read and print
+ * read_textfile - Reads a text file and prints it to POSIX stdout
+ * @filename: Name of the file
+ * @letters: Number of letters to read and print
  *
- * Return: actual number of characters read and printed, or 0 on failure
+ * Return: Number of letters printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+	ssize_t r, w;
+	char *buf;
 
-	/* Əgər filename NULL-dursa */
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	/* Faylı aç */
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-
-	/* Yaddaş ayır */
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
 	{
-		close(fd);
+		free(buf);
 		return (0);
 	}
 
-	/* Faylı oxu */
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+	r = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
-	/* Oxunanı ekrana yaz */
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	/* Təmizləmə */
-	free(buffer);
+	free(buf);
 	close(fd);
 
-	return (bytes_written);
+	if (r == -1 || w != r)
+		return (0);
+
+	return (w);
 }
+
